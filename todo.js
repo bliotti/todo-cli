526d26f04a80cb1391be37b1eeaeac047de43ef5
Brian Liotti <bdliotti@gmail.com>
2018-06-07 16:39:26 -0400

    accepting cli args

--- todo.js
+++ todo.js
@@ -1,6 +1,16 @@
-const R = require('ramda')
+const { head } = require('ramda')
 
+const [execPath, jsPath, cmd, ...rest] = process.argv
 
-const stuff = ['ffoo', 8 , 9 , 'bar']
+const handleCmd = cmd => {
+	switch (cmd) {
+		case 'add':
+			return 'add'
+		case 'rm':
+			return 'remove'
+		default:
+			return `${cmd} is not supported`
+	}
+}
 
-console.log(R.head(stuff))
+console.log(handleCmd(cmd))
